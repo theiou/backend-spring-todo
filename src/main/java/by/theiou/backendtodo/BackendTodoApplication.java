86ace3693084ac8ee22ba06ef8d1e9d521a9a998
theiou <temicho_0@mail.ru>
2022-07-26 01:46:52 +1000

    add start template, db properties and ssl security

---
+++ src/main/java/by/theiou/backendtodo/BackendTodoApplication.java
@@ -0,0 +1,13 @@
+package by.theiou.backendtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/theiou/backendtodo/BackendTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.theiou.backendtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
